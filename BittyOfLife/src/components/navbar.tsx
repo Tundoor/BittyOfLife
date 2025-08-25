92b8b1601ad401a06ded69817ebe833834e442fe
Thando <thandwem045@gmail.com>
2025-08-25 13:50:18 +0200

    Added styling for navbar

--- BittyOfLife/src/components/navbar.tsx
+++ BittyOfLife/src/components/navbar.tsx
@@ -1,12 +1,21 @@
 
 function Navbar() {
    return (<>
-    <img src="assets/Logo.png"></img>
+   <div className="icon-div">
+    <img src="assets/Logo.png" className="main-icon"></img>
+    
+    <div className="social-media-links">
+     <img src="assets/logos_pinterest.svg" className="social-icon"></img>
+     <img src="assets/logos_twitter.svg" className="social-icon"></img>
+     <img src="assets/skill-icons_instagram.svg" className="social-icon"></img>
+     
+    </div>
+    </div>
     <nav className="nav-div">
         <div className="nav-link-div">
-        <a className="home-link">Home</a>
-        <a className="blogs-link">Blogs</a>
-        <a className="post-link">Post</a>
+        <a className="nav-link"id="home-link">Home</a>
+        <a className="nav-link" id="blogs-link">Blogs</a>
+        <a className="nav-link" id="post-link">Post</a>
         </div>
     </nav>
    </>)
